4000cbcb2b99486a56dcc116be5fbf0c767aaf02
ahong456 <108887362+ahong456@users.noreply.github.com>
2024-08-09 14:43:53 +0800

    Add files via upload

---
+++ docs/student-js/student.js
@@ -0,0 +1,32 @@
+// id 值不能重复，必须保证是唯一值
+export const studentData = [{
+  id: 1,
+  name: '张三'
+},{
+  id: 2,
+  name: '李四'
+},{
+  id: 3,
+  name: '小红'
+},{
+  id: 4,
+  name: '小哄哄小哄哄小哄哄小哄哄'
+},{
+  id: 5,
+  name: '小张张'
+},{
+  id: 6,
+  name: '小张张'
+},{
+  id: 7,
+  name: '小张张'
+},{
+  id: 8,
+  name: '小张张'
+},{
+  id: 9,
+  name: '小张张'
+},{
+  id: 10,
+  name: '小张张'
+}]
